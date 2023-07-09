db0c5bf0e3314af32f11b251d0e27537a4565173
fox <fernandodiego.sn@gmail.com>
2023-07-09 12:32:18 -0300

    Embedded e Embeddable. Curso Java com JPA hibernate Alura

--- src/main/java/br/com/alura/loja/modelo/Cliente.java
+++ src/main/java/br/com/alura/loja/modelo/Cliente.java
@@ -1,51 +1,45 @@
 package br.com.alura.loja.modelo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "clientes")
 public class Cliente {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String nome;
-	private String cpf;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	public Cliente(String nome, String cpf) {
-		this.nome = nome;
-		this.cpf = cpf;
-	}
+    @Embedded
+    private DadosPessoais dadosPessoais;
 
-	public Cliente() {
-	}
 
-	public Long getId() {
-		return id;
-	}
+    public Cliente(String nome, String cpf) {
+        this.dadosPessoais = new DadosPessoais(nome, cpf);
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Cliente() {
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public DadosPessoais getDadosPessoais() {
+        return dadosPessoais;
+    }
 
-	public String getCpf() {
-		return cpf;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
+    public String getNome() {
+        return this.dadosPessoais.getNome();
+    }
+
+    public String getCPF() {
+        return this.dadosPessoais.getCpf();
+    }
+
 
 }
